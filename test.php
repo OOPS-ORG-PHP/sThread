6a3decb0b3c2e034e21251c212bfb7a4f9a5c123
JoungKyun Kim <joungkyun@daum.net>
2010-02-18 04:52:40 +0000

    add comments for host format

--- test.php
+++ test.php
@@ -1,9 +1,33 @@
 <?php
+error_reporting (E_ALL & ~E_NOTICE);
+/*
+ * For over PHP 5.3
+ */
+if ( version_compare (PHP_VERSION, '5.3.0') >= 0 ) {
+	//error_reporting (E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+	date_default_timezone_set ('Asia/Seoul');
+}
+
 if ( ! extension_loaded ('libevent') ) {
 	dl ('libevent.so');
 }
 
+require_once 'sThread.php';
+
 $s = new sThread;
+$s->async = false;
+ePrint::$debugLevel = 0;
+
+/*
+ * Host Format
+ * SERVER:PORT:MODULE
+ * SERVER:PORT
+ * SERVER:PORT:MODULE|EXTRA_OPTION
+ * SERVER:PORT|EXTRA_OPTION
+ *
+ * Extra Option Format
+ * var1=>val1,var2=>val2,var3=>val3...
+ */
 
 $host = 'test.domain.com:80';
 /*
