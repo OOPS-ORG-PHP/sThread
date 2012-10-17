8408333127546f9892f1e4158aff79880f32affe
oops <oops@32a2f610-65cd-11e0-b701-aafacf67e33a>
2012-10-17 14:00:23 +0000

    objectCopy, varCopy method 추가

--- trunk/sThread/Vari.php
+++ trunk/sThread/Vari.php
@@ -1,6 +1,5 @@
 <?php
 /**
- *
  * Common Variables of Single Thread Monitoring<br>
  * File: sThread/Vari.php
  *
@@ -14,10 +13,43 @@
  * @link        http://pear.oops.org/package/sThread
  * @filesource
  */
+
+/**
+ * sThread package에서 사용하는 공통 변수와 변수 관련
+ * 메소드를 제공
+ *
+ * @category    Network
+ * @package     sThread
+ * @subpackage  sThread_CORE
+ * @author      JoungKyun.Kim <http://oops.org>
+ * @copyright   1997-2012 OOPS.ORG
+ * @license     BSD License
+ * @version     $Id$
+ * @link        http://pear.oops.org/package/sThread
+ * @filesource
+ */
 Class Vari {
 	// {{{ properties
+	/**
+	 * 세션 관련 정보 저장
+	 *
+	 * @access public
+	 * @var    object
+	 */
 	static public $sess;
+	/**
+	 * 옵션값 저장
+	 *
+	 * @access public
+	 * @var    object
+	 */
 	static public $opt;
+	/**
+	 * 처리 결과값을 저장
+	 *
+	 * @access public
+	 * @var    object
+	 */
 	static public $res;
 
 	const DEBUG1 = 1;
@@ -31,6 +63,59 @@ Class Vari {
 	const EVENT_SEND_DONE   = 1;
 	const EVENT_READY_CLOSE = 1000;
 	const EVENT_UNKNOWN     = false;
+	// }}}
+
+	// {{{ (void) Vari::objectCopy (&$r, $obj)
+	/**
+	 * 2번째 파라미터에 주어진 값을 다른 메모리 주소에
+	 * cpoy하여 1번째 변수에 할당한다.
+	 *
+	 * @access public
+	 * @return void
+	 * @param  mixed 빈 변수
+	 * @param  mixed 원본 변수
+	 */
+	function objectCopy (&$r, $obj) {
+		switch (($type = gettype ($obj))) {
+			case 'object' :
+			case 'array' :
+				foreach ( $obj as $key => $val ) {
+					switch (($subtype = gettype ($val))) {
+						case 'array' :
+						case 'object' :
+							if ( $type == 'array' ) {
+								$r[$key] = null;
+								self::objectCopy ($r[$key], $val);
+							} else {
+								$r->$key = null;
+								self::objectCopy ($r->$key, $val);
+							}
+							break;
+						default:
+							if ( $type == 'array' )
+								$r[$key] = self::varCopy ($val);
+							else
+								$r->$key = self::varCopy ($val);
+					}
+				}
+				break;
+			default :
+				$r = self::varCopy ($val);
+		}
+	}
+	// }}}
+
+	// {{{ (mixed) Vari::varCopy ($var)
+	/**
+	 * 입력된 값을 다른 주소의 메모리에 copy하여 반환
+	 *
+	 * @access public
+	 * @return mixed
+	 * @param  mixed
+	 */
+	function varCopy ($var) {
+		return $var;
+	}
 	// }}}
 }
 ?>
